1bec9c65a38d3ab8b19cc94eef611e90943c7c3f
sam.drummond <sam.drummond@gmail.com>
2015-04-24 09:57:00 +1200

    SD: Updated so use the results widget inherits from the MapGallerSearch.js file in its own directory

--- widgets/Gallery/widgets/Result/widget.js
+++ widgets/Gallery/widgets/Result/widget.js
@@ -2,12 +2,14 @@ define([
 	'dojo/_base/declare',
 	'dojo/_base/array',
 	'dojo/_base/lang',
+	'dojo/query',
 	'dijit/_WidgetBase',
 	'dijit/_TemplatedMixin',
-	'./../../js/MapGallerySearch',
+	'./js/MapGallerySearch', //Need to rework this a little.
 	'./../Pagination/widget',
+	'./../ItemThumb/Widget',
 	'dojo/text!./template/widget.html'
-	],function(declare, arrayUtil, lang, _WidgetBase, _TemplatedMixin, MapGallerySearch, Pagination, widgetTemplate){
+	],function(declare, arrayUtil, lang, query, _WidgetBase, _TemplatedMixin, MapGallerySearch, Pagination, ItemThumb, widgetTemplate){
 	
 		return declare('ResultsWidget', [_WidgetBase, _TemplatedMixin, MapGallerySearch],{
 			templateString:widgetTemplate,
@@ -15,26 +17,35 @@ define([
 			_pagination:null,
 			pageSize:0,
 			page:1,
+			_resultsContainer:null,
 			startup:function(){
 				this.inherited(arguments);
+				this._resultsContainer = query('.gallery-results-container', this.domNode)[0];//only one so grab the first
 			},
 			getAllMapsAndApps:function(){
 				this.requestSearchResults();
 			},
 			searchResultsRequestResponse:function(results){
-				this._webMaps = results.WebMaps;
-				this._displayThumbs();
+				this._displayThumbs(results.WebMaps);
+				this._configurePagination(results.TotalHits);
+			},
+			_configurePagination:function(totalHits){
 				
 				var pagination = new Pagination();
-				pagination.totalResults = results.TotalHits;
+				pagination.totalResults = totalHits;
 				pagination.resultsPerPage = this.pageSize;
 				pagination.currentPage = this.page;
 				pagination.pagesPerSide = 1;
 				pagination.placeAt(this.domNode, "last");
 				pagination.on("page", lang.hitch(this, this._updateDisplayThumbs));
-			},
-			_displayThumbs:function(){
 				
+			},
+			_displayThumbs:function(webMaps){
+				arrayUtil.forEach(webMaps, lang.hitch(this, this._displayThumb));
+			},
+			_displayThumb:function(webMap){
+				var itemThumb = new ItemThumb();
+				itemThumb.placeAt(this._resultsContainer, 'last');
 			},
 			_updateDisplayThumbs:function(/*Event*/ e){
 				console.log("Need to update the thumbs to show page " + e);
